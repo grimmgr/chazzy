6da0bc3afbc1b5adf69f6ff55c301fe3a993798d
grimmgr <ggrimm33@gmail.com>
2020-11-11 12:10:31 -0800

    style homepage

--- client/src/components/Cover/index.js
+++ client/src/components/Cover/index.js
@@ -4,15 +4,16 @@ import './coverStyle.css';
 export const Cover = () => {
     return (
         <div className='cover'>
-            <h1>CHASTITY BELT</h1>
+            <p>Self-titled album out now on Hardly Art!</p>
             <img src='images/album_cover.jpg' alt='album cover' />
-            <p>Self-titled album out now on Hardly Art</p>
-            <a href='https://ffm.to/chazzy' target='_blank' rel='noopener noreferrer'>Listen</a>
-            <span> | </span>
-            <a href='https://www.hardlyart.com/releases/chastity_belt/chastity_belt' target='_blank' rel='noopener noreferrer'>Buy</a>
-            <br />
-            <a href='https://www.patreon.com/chastitybelt' target='_blank' rel='noopener noreferrer'>Join our Patreon ;)</a>
-            <p>merch merch merch merch</p>
+            <div className='buy'>
+                <a className='barcode' href='https://www.hardlyart.com/releases/chastity_belt/chastity_belt' target='_blank' rel='noopener  noreferrer'>BUY</a>
+            </div>
+            
+            <div className='listen-patreon'>
+                <a id='listen' href='https://ffm.to/chazzy' target='_blank' rel='noopener noreferrer'>Listen</a>
+                <a id='patreon' href='https://www.patreon.com/chastitybelt' target='_blank' rel='noopener noreferrer'>Join our Patreon <span className='wink'>;)</span></a>
+            </div>
         </div>
     );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import { Cover } from '../components/Cover';
 export const HomePage = () => {
     return (
         <section id='home'>
+            <h1>CHASTITY BELT</h1>
             <Cover />
         </section>
     );
