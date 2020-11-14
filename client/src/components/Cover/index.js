9fc6b4523d001241f9813e9e8b41f9f8d4a6edfe
grimmgr <ggrimm33@gmail.com>
2020-11-14 14:25:12 -0800

    style homepage

--- client/src/components/Cover/index.js
+++ client/src/components/Cover/index.js
@@ -6,13 +6,20 @@ export const Cover = () => {
         <div className='cover'>
             <h1>CHASTITY BELT</h1>
             <p>Self-titled album out now on Hardly Art!</p>
+            <div className='listen'>
+                <a href='https://ffm.to/chazzy' target='_blank' rel="noopener noreferrer">
+                    <p>LISTEN</p>
+                </a>
+            </div>
             <img className='album-cover' src='images/album_cover.jpg' alt='album cover' />
             <div className='buy'>
                 <a className='barcode' href='https://www.hardlyart.com/releases/chastity_belt/chastity_belt' target='_blank' rel='noopener  noreferrer'>BUY</a>
             </div>
             
             <div className='patreon'>
-                <a id='patreon' href='https://www.patreon.com/chastitybelt' target='_blank' rel='noopener noreferrer'>Join our Patreon <span className='wink'>;)</span></a>
+                <a href='https://www.patreon.com/chastitybelt' target='_blank' rel='noopener noreferrer'>
+                    <p>Join our Patreon</p>
+                    </a>
             </div>
         </div>
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Cover } from '../components/Cover';
-import { Listen } from '../components/Listen';
 
 export const HomePage = () => {
     return (
         <section id='home'>
-            <Listen />
             <Cover />
         </section>
     );
