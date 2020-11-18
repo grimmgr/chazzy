481c530226bda801ea61bedfe8f329d68f5f59a1
grimmgr <ggrimm33@gmail.com>
2020-11-18 15:05:32 -0800

    nav animation

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
-// import { NavButton } from '../components/NavButton';
+import { CSSTransition } from 'react-transition-group';
 import { Nav } from '../components/Nav';
 import '../pageStyles/menuStyle.css';
 
 export const Menu = () => {
-    const [menuState, setMenuState] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const navButtonClick = (e) => {
         e.preventDefault();
-        setMenuState(!menuState)
+        setOpen(!open)
     };
 
     return (
         <section id='menu'>
-            { menuState ? <Nav /> : null}
-                
-            <div className='nav-button' onClick={ navButtonClick }>
-                <span className='top'></span>
-                <span className='middle'></span>
-                <span className='bottom'></span>
-            </div>
+            <CSSTransition
+            in={open}
+            timeout={500}
+            classNames={'open-nav'}
+            unmountOnExit
+            >
+            <Nav />
+            </CSSTransition>
+            <CSSTransition
+            in={!open}
+            timeout={500}
+            classNames={'close-btn'}
+            >  
+                <div className='nav-button' onClick={ navButtonClick }>
+                    <span className='top'></span>
+                    <span className='middle'></span>
+                    <span className='bottom'></span>
+                </div>
+            </CSSTransition>
         </section>
     );
 };
