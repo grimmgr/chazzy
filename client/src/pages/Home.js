0b2c3dc1b7c4572ed477837450b9954f9fcb9481
grimmgr <ggrimm33@gmail.com>
2020-11-21 22:59:33 -0800

    home style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,15 +5,16 @@ import '../pageStyles/homeStyle.css';
 export const Home = () => {
     return (
         <section id='home'>
-            <h1>CHASTITY BELT</h1>
-            <div className='cover'>
-                <p>Self-titled album out now on Hardly Art!</p>
-                <div className='listen link'>
-                    <a href='https://ffm.to/chazzy' target='_blank' rel="noopener noreferrer">
-                        <p>LISTEN</p>
-                    </a>
+            <div class='home-title'>
+                <h1>CHASTITY BELT</h1>
+                <div className='listen'>
+                <a href='https://ffm.to/chazzy' target='_blank' rel="noopener noreferrer">
+                    <p>LISTEN</p>
+                </a>
                 </div>
-                <img className='album-cover' src='images/album_cover.jpg' alt='album cover' />
+            </div>
+            <div className='cover'>
+                <img className='album-cover' src='images/album_cover.png' alt='album cover' />
                 <div className='buy'>
                     <a className='barcode' href='https://www.hardlyart.com/releases/chastity_belt/chastity_belt' target='_blank' rel='noopener  noreferrer'>BUY</a>
                 </div>
