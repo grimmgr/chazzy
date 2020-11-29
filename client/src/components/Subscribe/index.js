f18c1731998dba6d0866aa32f5166893f93806d7
grimmgr <ggrimm33@gmail.com>
2020-11-29 13:13:31 -0800

    subscribe cheers

--- client/src/components/Subscribe/index.js
+++ client/src/components/Subscribe/index.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CSSTransition } from 'react-transition-group';
-import { JoinMsg } from '../JoinMsg';
 import './subscribeStyle.css';
 
 export const Subscribe = () => {
     const [email, setEmail] = useState('');
-    const [submitted, setSubmitted] = useState(false);
     const [submitStatus, setSubmitStatus] = useState('');
-    // const [yay, setYay] = useState('');
+    const [yay, setYay] = useState(false);
 
     const subscribe = (e) => {
         e.preventDefault();
@@ -22,6 +20,7 @@ export const Subscribe = () => {
             if ( response.data.status === 400 ) {
                 if ( response.data.title === 'Member Exists') {
                     setSubmitStatus('alreadyMember');
+                    setYay(true);
                     setEmail('');
                 } else if ( response.data.title === 'Invalid Resource') {
                     setSubmitStatus('invalidEmail');
@@ -30,45 +29,74 @@ export const Subscribe = () => {
                 }
             } else {
                 setEmail('');
-                setSubmitStatus('yay');
+                setSubmitStatus('joined');
+                setYay(true);
             }
 
         })
         .catch(err => setSubmitStatus(err));
     }
 
-    useEffect(() => {
-        if ( submitStatus ) {
-            setSubmitted(true);
-        }
-        
-    }, [submitStatus])
-
     return (
         <section id='subscribe' className='subscribe-container'>
             <div className='subscribe'>
-            <h3>JOIN OUR MAILING LIST</h3>
-            <form>
-                <input 
-                    type='email'
-                    placeholder='enter email address' 
-                    required pattern='\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
-                    value={email}
-                    onChange={ (e) => setEmail(e.target.value)}
-                    />
-                <button
-                    onClick={subscribe} 
-                    className='subscribe-btn'>join</button>
-            </form>
-            <CSSTransition
-            in={submitted}
-            timeout={5000}
-            classNames={'join-msg'}
-            unmountOnExit
-            >
-                <JoinMsg status={submitStatus} />
-            </CSSTransition>
+                <h3>JOIN OUR MAILING LIST</h3>
+                <form>
+                    <input 
+                        type='email'
+                        placeholder='enter email address' 
+                        required pattern='\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
+                        value={email}
+                        onChange={ (e) => setEmail(e.target.value)}
+                        />
+                    <button
+                        onClick={subscribe} 
+                        className='subscribe-btn'>join</button>
+                </form>
+                <CSSTransition
+                    in={( submitStatus === 'alreadyMember' )}
+                    timeout={500}
+                    classNames={'join-msg'}
+                    unmountOnExit
+                >
+                    <p className='join-msg'>looks like you're already on our mailing list!</p>
+                </CSSTransition>
+                <CSSTransition
+                    in={( submitStatus === 'invalidEmail' )}
+                    timeout={500}
+                    classNames={'join-msg'}
+                    unmountOnExit
+                >
+                    <p className='join-msg'>please enter a valid email address</p>
+                </CSSTransition>
+                <CSSTransition
+                    in={( submitStatus === 'cantProcess' )}
+                    timeout={500}
+                    classNames={'join-msg'}
+                    unmountOnExit
+                >
+                    <p className='join-msg'>hmmm something's up... double check you've entered a valid email address and if it's still not working shoot us an email at beltchastity@gmail.com</p>
+                </CSSTransition>
+                <CSSTransition
+                    in={( submitStatus === 'joined' )}
+                    timeout={500}
+                    classNames={'join-msg'}
+                    unmountOnExit
+                >
+                    <p className='join-msg'>Cheers! We'll be in touch ;)</p>
+                </CSSTransition>
             </div>
+            <CSSTransition
+                in={yay}
+                timeout={500}
+                classNames={'cheers'}
+                unmountOnExit
+            >
+                <div className='cheers'>
+                    <img className='smiley' src='images/smiley_pink.png'></img>
+                    <img className='smiley' src='images/smiley_pink.png'></img>
+                </div>
+            </CSSTransition>
         </section>
     );
 };
