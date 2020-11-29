b9b04aa32c4ce7b9896198029076445b6ccc1c99
grimmgr <ggrimm33@gmail.com>
2020-11-28 17:28:55 -0800

    mailchimp

--- client/src/components/Subscribe/index.js
+++ client/src/components/Subscribe/index.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './subscribeStyle.css';
 
 export const Subscribe = () => {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [yay, setYay] = useState('');
+    console.log(`error: ${error}`);
+    console.log(`yay: ${yay}`);
+
+    const subscribe = (e) => {
+        e.preventDefault();
+        axios.post('/subscribe', {
+            email_address: email,
+            status: 'subscribed'
+        })
+        .then((response) => {
+            console.log(response);
+            if ( response.data.status === 400 ) {
+                if ( response.data.title === 'Member Exists') {
+                    setError('alreadyMember');
+                } if ( response.data.title === 'Invalid Resource') {
+                    setError('invalidEmail');
+                } else {
+                    setError('cantProcess')
+                }
+            } else {
+                setYay('yay');
+            }
+
+        })
+        .catch(err => setError(err));
+    }
+
+
+
     return (
         <section id='subscribe' className='subscribe-container'>
             <div className='subscribe'>
             <h3>JOIN OUR MAILING LIST</h3>
-            <form >
-                <input type='text' placeholder='enter email address'/>
-                <input className='subscribe-btn' type='submit' value='join'/>
+            <form>
+                <input 
+                    type='email'
+                    placeholder='enter email address' 
+                    required pattern='\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
+                    value={email}
+                    onChange={ (e) => setEmail(e.target.value)}
+                    />
+                <button
+                    onClick={subscribe} 
+                    className='subscribe-btn'>join</button>
             </form>
             </div>
         </section>
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const passport = require('passport');
 const path = require('path');
+const axios = require('axios');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.post('/tehe', passport.authenticate('local'), (req, res) => 
     res.json(req.body));
+
+router.post('/subscribe', (req, res) => {
+    axios.post('https://us7.api.mailchimp.com/3.0/lists/3cc5a7c1c0/members', req.body, {
+        headers: {
+            'Authorization': 'Basic 64e00b0fc6345847583eb3ff01ba36d2-us7'
+        }
+    })
+    .then(response => {
+        res.json(response.data)
+    })
+    .catch(err => {
+        res.json(err.response.data)
+    });
+});
 
 router.use('/api', apiRoutes);
 
