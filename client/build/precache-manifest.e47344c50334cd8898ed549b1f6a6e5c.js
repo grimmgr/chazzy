f6da79d1dc16d44a0cf052c0a35f30486c93058b
grimmgr <ggrimm33@gmail.com>
2020-11-30 14:29:31 -0800

    build

---
+++ client/build/precache-manifest.e47344c50334cd8898ed549b1f6a6e5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9275d7930809515bbf63",
+    "url": "/static/css/main.7b9344c9.chunk.css"
+  },
+  {
+    "revision": "9275d7930809515bbf63",
+    "url": "/static/js/main.54378ba3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db08b9d9f82a5ce23035",
+    "url": "/static/js/2.b90ee8bf.chunk.js"
+  },
+  {
+    "revision": "8165385ab17bef176c19db2d9d9c2f8a",
+    "url": "/static/media/purp.8165385a.png"
+  },
+  {
+    "revision": "0a4f4f2bf13903a2cdefce702c3a9762",
+    "url": "/static/media/nav_background.0a4f4f2b.png"
+  },
+  {
+    "revision": "3202f8df37eb6eadf83841b3fef11142",
+    "url": "/static/media/smiley_multi.3202f8df.png"
+  },
+  {
+    "revision": "302ab9747c9f6d0b505fb3c147d134eb",
+    "url": "/static/media/selfie.302ab974.JPG"
+  },
+  {
+    "revision": "a7b97166e27bac1ae4aa94bc233dac2b",
+    "url": "/static/media/scheana2.a7b97166.jpg"
+  },
+  {
+    "revision": "1fc01da57c71d44ab7fb82e777a91f16",
+    "url": "/static/media/leaves.1fc01da5.png"
+  },
+  {
+    "revision": "e8942436add207a313a1eb87ea13a0a3",
+    "url": "/static/media/home_mobile.e8942436.png"
+  },
+  {
+    "revision": "e672cafbc8fde0918c96edef57f99019",
+    "url": "/index.html"
+  }
+];
