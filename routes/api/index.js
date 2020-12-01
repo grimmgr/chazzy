50a70619283a70d1040fcd6857b31f729c181485
grimmgr <ggrimm33@gmail.com>
2020-12-01 13:13:00 -0800

    troubleshoot

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,12 +6,9 @@ require('dotenv').config();
 router.use('/fan-art', fanArtRoutes);
 
 router.get('/ig/:post', (req, res) => {
-    console.log('get ig info!');
-    const igToken = process.env.IG_TOKEN;
+    // const igToken = process.env.IG_TOKEN;
     const post = req.params.post;
-    console.log(igToken);
-    console.log(post);
-    axios.get(`https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/${post}/&omitscript=true&hidecaption=true&access_token=${igToken}`)
+    axios.get(`https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/${post}/&omitscript=true&hidecaption=true&access_token=204761497824117|swYmFWa5sv52UWrzf2p-3z9kktw`)
     .then(response => {
         const igInfo = {
             author: response.data.author_name,
