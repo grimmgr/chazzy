f853dae8e29e29af939218a78939cfcd650d41d9
grimmgr <ggrimm33@gmail.com>
2020-12-08 17:13:36 -0800

    fan art

--- client/src/components/NoEvents/index.js
+++ client/src/components/NoEvents/index.js
@@ -8,7 +8,7 @@ export const NoEvents = () => {
             <div className='no-event'>
                 <div className='no-event-note'>
                     <p className='venue'>We don't have anything planned at the moment ;(</p>
-                    <p className='updates'>join our mailing list below for updates! <span className='se-arrow'>&#8600;</span></p>
+                    <p className='updates'>Join our mailing list below for updates! <span className='se-arrow'>&#8600;</span></p>
                 </div>
             </div>
         </div>
